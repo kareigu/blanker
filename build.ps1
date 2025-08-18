29915cc6b4472d239ef4b96a00e799021487e35a
karei <mail@karei.dev>
2025-08-18 21:49:06 +0300

    support `cl` and `clang-cl`

--- build.ps1
+++ build.ps1
@@ -2,12 +2,13 @@ $entry = "main.c"
 $rc = "blanker.rc"
 $res = "blanker.res"
 $output = "blanker.exe"
-$libs = @("user32","gdi32") | % { "-l$_"}
 $warnings = "-Wall","-Wextra","-Werror"
+$libs = "user32","gdi32"
 $verbose = ""
 $run = 0
-$opt = "-g"
+$release = 0
 $rc_exe = "rc"
+$cc = "clang"
 
 $i = 0
 foreach ($arg in $args) {
@@ -19,10 +20,11 @@ foreach ($arg in $args) {
         rm -force *.res
         rm -force *.rdi
         rm -force *.ilk
+        rm -force *.obj
 
         exit 0
     }
-    elseif ($arg -eq "--release") { $opt = "-O3" }
+    elseif ($arg -eq "--release") { $release = 1 }
     elseif ($arg -eq "--rc") { 
         if ($args.Length -gt $i + 1) {
             $rc_exe = "$($args[$i + 1])" 
@@ -31,7 +33,65 @@ foreach ($arg in $args) {
             exit 1
         }
     }
+    elseif ($arg -eq "--cc") { 
+        if ($args.Length -gt $i + 1) {
+            $cc = "$($args[$i + 1])" 
+        } else {
+            echo "no path provided to --cc"
+            exit 1
+        }
+    }
     $i++
+}
+
+function check_cc {
+    if ($cc -eq "clang-cl") {
+        return $true
+    } elseif ($cc -eq "cl") {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+function format_opts {
+    if ($release -eq 1) {
+        if (check_cc -eq $true) {
+            return "/O2"
+        } else {
+            return "-O2"
+        }
+    } else {
+        if (check_cc -eq $true) {
+            return ""
+        } else {
+            return "-g"
+        }
+    }
+}
+
+function format_warnings {
+    if (check_cc -eq $true) {
+        return "/W4","/WX"
+    } else {
+        return $warnings
+    }
+}
+
+function format_libs {
+    if (check_cc -eq $true) {
+        return $libs | % { "$_.lib" } 
+    } else {
+        return $libs | % { "-l$_" } 
+    }
+}
+
+function format_output {
+    if (check_cc -eq $true) {
+        return @("/Fe:$output")
+    } else {
+        return "-o","$output"
+    }
 }
 
 echo "Entry: $entry"
@@ -46,9 +106,16 @@ try {
     exit 1
 }
 
+try {
+    $_ = Get-Command $cc -ErrorAction Stop
+} catch {
+    echo "$cc not found in path."
+    exit 1
+}
+
 & $rc_exe "$rc"
 
-clang "$entry" $res $opt $verbose @warnings @libs -o $output
+& $cc "$entry" "$res" @(format_opts) @(format_warnings) @(format_libs) @(format_output)
 
 if ($run -eq 1) {
     & ".\$output"
